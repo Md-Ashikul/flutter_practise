93e30117a39d58e316c050c2ed41a2ca715b25b5
Md. Ashikul Islam <07ashikul@gmail.com>
2023-09-16 22:31:52 +0600

    Grid View

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,59 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text("My first App"),
+void main ( ) {
+  runApp(myApp()) ;
+}
+
+class myApp extends StatelessWidget {
+  const myApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: true,
+      home: Homescreen( ),
+      title: "Smart APP",
+    ) ;
+  }
+}
+
+class Homescreen extends StatelessWidget {
+  const Homescreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.cyan,
+        leading: Icon(Icons.add_business),
+        toolbarHeight: 100,
+        title: Text("Home"),
+        centerTitle: true,
+      ),
+      body: Scrollbar(
+        thickness: 10,
+        radius: Radius.circular(10),
+        child: GridView(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+          children: [
+            Icon(Icons.adb_outlined),
+            Icon(Icons.adb_outlined),
+            Icon(Icons.adb_outlined),
+            Icon(Icons.adb_outlined),
+            Icon(Icons.adb_outlined),
+            Icon(Icons.adb_outlined),
+            Icon(Icons.adb_outlined),
+            Icon(Icons.adb_outlined),
+            Icon(Icons.adb_outlined),
+            Icon(Icons.adb_outlined),
+            Icon(Icons.adb_outlined),
+            Icon(Icons.adb_outlined),
+
+          ],
         ),
       ),
-    )
-  );
+    );
+  }
 }
+
 
