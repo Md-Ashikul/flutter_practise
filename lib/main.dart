26ce2ac5a3b9dfb1141dcaaf09bd16b4204a2541
Md. Ashikul Islam <07ashikul@gmail.com>
2023-09-16 10:50:07 +0600

    commit updated and push

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,30 @@
-//abcdefg
+import 'package:flutter/material.dart';
+void main ( ) {
+  runApp(myApp()) ;
+}
+
+class myApp extends StatelessWidget {
+  const myApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: true,
+      home: HomeScreen( ),
+      title: 'Ostad',
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super (key:key) ;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home")
+      ),
+    );
+  }
+}
+
